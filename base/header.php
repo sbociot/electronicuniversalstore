be928adcdf960f1e19b382ddaca50348b8429048
dchavezc <dervys75@gmail.com>
2017-02-24 14:41:29 -0500

    ajust images, et favico

--- base/header.php
+++ base/header.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <title>Electronic Universal Store | Acceuil</title>
     <link rel="stylesheet" href="style/master.css">
+    <link href="../images/electronicuniversalstore.png" rel="icon" type="image/x-icon"/>
   </head>
   <body>
       <?php require_once 'base/menu.php'; ?>
---
+++ promo.php
@@ -0,0 +1,27 @@
+<?php
+require_once 'acces/acces_serv.php';
+var_dump($categories);
+define('ARTICLE_IMG_PATH', 'images/');
+?>
+
+    <div id="promotion">
+
+<?php foreach ($categories as $id => $article) { ?>
+    <figure>
+        <h2><?= $article['nom']?></h2>
+        <img src="<?= ARTICLE_IMG_PATH,$article['image']?>" alt="" />
+        <figcaption><?= utf8_encode($article['description'])?></figcaption>
+
+        <h4>Prix:</h4>
+        <p><?= $article['prix']?></p>
+        <h4>Prix avec taxes:</h4>
+        <p><?= $article['prix avec taxes']?></p>
+        <h3>Economisez:</h3>
+        <p><?= $article['economiser']?></p>
+        <h4>Livraison:</h4>
+        <p><?= $article['livraison']?></p>
+
+    </figure>
+
+    </div>
+<?php }?>
