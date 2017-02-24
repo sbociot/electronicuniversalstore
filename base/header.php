1e08c5dc8774458a8a74b3d2654e22d500d4c96f
SteveBocicot <Steve@stevemac.isimtl.local>
2017-02-24 08:29:16 -0500

    "modif des fichier de base changer le dossier css"

--- base/header.php
+++ base/header.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Electronic Universal Store | Acceuil</title>
-    <link rel="stylesheet" href="css/master.css">
+    <link rel="stylesheet" href="style/master.css">
   </head>
   <body>
       <?php require_once 'base/menu.php'; ?>
