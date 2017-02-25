c359b423e8e43f47b12c4c5f2fce9e555d1ac16b
dchavezc <dervys75@gmail.com>
2017-02-24 22:16:57 -0500

    ajust images, et favico
    add menu, images
    
    imagen favico

--- base/header.php
+++ base/header.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <title>Electronic Universal Store | Acceuil</title>
     <link rel="stylesheet" href="../style/master.css">
+    <link rel="stylesheet" href="../images/favico/favicon-32x32.png" type="image/x-icon">
   </head>
   <body>
       <?php require_once 'base/menu.php'; ?>
