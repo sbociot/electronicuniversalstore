9bb0ac057b854447977eafffdff43b712c1e1fed
dchavezc <dervys75@gmail.com>
2017-02-27 14:13:34 -0500

    "RETOUCHE FOOTERl"

--- base/footer.php
+++ base/footer.php
@@ -8,13 +8,13 @@
                 <li>Montréal (Québec)</li>
                 <li>H2M 1M2</li>
                 <li>514 842-2426</li>
+                <li>info@centre-dentaire-de-l-est.com</li>
             </ul>
             <a href="https://goo.gl/maps/nUuwptRP9mH2">
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2794.3027433709494!2d-73.64289478443992!3d45.54423507910192!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cc918e0c061b07f%3A0x647a6b6d7cb681a7!2sISI%2C+l&#39;Institut+sup%C3%A9rieur+d&#39;informatique!5e0!3m2!1sfr!2sca!4v1487356742365"
                     width="80%" height="100px" frameborder="0" style="border:0" allowfullscreen></iframe>
             </a>
-            <p>info@centre-dentaire-de-l-est.com</p>
         </article>
     <article class="social">
 
