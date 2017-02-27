dbebdf6fec1a126a5bd5383e1e99346c0720204e
dchavezc <dervys75@gmail.com>
2017-02-27 10:11:08 -0500

    "basse doner promo.sql"

--- base/footer.php
+++ base/footer.php
@@ -2,26 +2,63 @@
 
 <footer>
       <div class="social">
-        <aside>
-            <h2><span itemprop="name">Centre Dentaire de l'Est</span></h2>
+        <article>
+            <h2>Nous Coordonnées</h2>
+            <h4><span itemprop="name">Electronic Universal Store</span></h4>
             <ul>
                 <li>255 Crémazie Est</li>
                 <li>Montréal (Québec)</li>
                 <li>H2M 1M2</li>
                 <li>514 842-2426</li>
-                <li>info@centre-dentaire-de-l-est.com</li>
             </ul>
-
+            <a href="https://goo.gl/maps/nUuwptRP9mH2"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2794.3027433709494!2d-73.64289478443992!3d45.54423507910192!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cc918e0c061b07f%3A0x647a6b6d7cb681a7!2sISI%2C+l&#39;Institut+sup%C3%A9rieur+d&#39;informatique!5e0!3m2!1sfr!2sca!4v1487356742365" width="80%" height="100px" frameborder="0" style="border:0" allowfullscreen></iframe></a>
             <p>© 2017, Electronic Universal Store / Tous droits réservés.</p>
-        </aside>
-        <aside></aside>
-        <aside>
-        <h3>Sociale ICONS</h3>
-        <a href="https://www.facebook.com/"><img src="images/redes/facebookicon.png" alt="facebook"></a>
-        <a href="https://www.instagram.com/"><img src="images/redes/instagram.png" alt="instagram"></a>
-        <a href="https://twitter.com/"><img src="images/redes/twitter.png" alt="twitter"></a>
-        <a href="https://www.youtube.com/"><img src="images/redes/youtube.png" alt="youtube"></a>
-        </aside>
+        </article>
+        <article>
+            <h2><span itemprop = "name" >Nos heures d'ouverture.</span></h2>
+            <table>
+                <tr>
+                    <th>Lundi:</th>
+                    <td>8h00 à 18h00</td>
+                </tr>
+                <tr>
+                    <th>Mardi:</th>
+                    <td>8h00 à 19h00</td>
+                </tr>
+                <tr>
+                    <th>Mercredi:</th>
+                    <td>8h00 à 17h00</td>
+                </tr>
+                <tr>
+                    <th>Jeudi:</th>
+                    <td>8h00 à 17h00</td>
+                </tr>
+                <tr>
+                    <th>Vendredi:</th>
+                    <td>FERMÉ</td>
+                </tr>
+                <tr>
+                    <th>Samedi:</th>
+                    <td>FERMÉ</td>
+                </tr>
+                <tr>
+                    <th>Dimanche:</th>
+                    <td>FERMÉ</td>
+                </tr>
+            </table>
+
+        </article>
+        <article>
+            <h3>SOUTIEN DE COMMANDE</h3>
+            <a href="../contact.php">Contactez nous</a>
+
+            <h3>Sociale ICONS</h3>
+            <a href="https://www.facebook.com/"><img src="images/redes/facebookicon.png" alt="facebook">Facebook</a>
+            <a href="https://www.instagram.com/"><img src="images/redes/instagram.png" alt="instagram">Instagram</a>
+            <a href="https://twitter.com/"><img src="images/redes/twitter.png" alt="twitter">Twitter</a>
+            <a href="https://www.youtube.com/"><img src="images/redes/youtube.png" alt="youtube">Youtube</a>
+            <p>info@centre-dentaire-de-l-est.com</p>
+        </article>
     </div>
 </footer>
 </body>
