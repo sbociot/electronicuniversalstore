2c0e26731916d6df1334851e008d70d79df91551
dchavezc <dervys75@gmail.com>
2017-02-28 10:01:47 -0500

    "footer ok ajuste de u-lil et css"

--- base/footer.php
+++ base/footer.php
@@ -28,18 +28,18 @@
             <li><a href="https://play.google.com/store/apps"><img src="images/redes/android-logo.png"
                                                                   alt="android-logo"></a>Appli pour Android
             </li>
+        </ul>
 
-            <div id="social">
-                <h3>Réseaux Sociaux</h3>
-                <ul id="redes">
+        <h3>Réseaux Sociaux</h3>
+        <ul id="redes">
                     <li><a href="https://www.facebook.com/"><img src="images/redes/facebookicon.png" alt="facebook"></a>
                     </li>
                     <li><a href="https://www.instagram.com/"><img src="images/redes/instagram.png" alt="instagram"></a>
                     </li>
                     <li><a href="https://twitter.com/"><img src="images/redes/twitter.png" alt="twitter"></a></li>
                     <li><a href="https://www.youtube.com/"><img src="images/redes/youtube.png" alt="youtube"></a></li>
-                </ul>
-            </div>
+        </ul>
+
 
     </div>
     <div class="horaires">
