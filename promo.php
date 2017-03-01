eae76aad9911b4b7ba9c45bede2ff2656a96b653
sbociot <stevebocicot@icloud.com>
2017-03-01 13:13:19 -0500

    “Promo modification”

--- promo.php
+++ promo.php
@@ -6,22 +6,26 @@ require_once'acces/acces_serv.php';
 ?>
 
     <div id="slider">
-<?php foreach ($categories as $id => $article) { ?>
 
+<?php foreach ($categories as $id => $article) { ?>
+    <a href="aubaine_choix.php?=promo=<?= utf8_encode($article['nom'])?>" >
     <figure>
         <img src="<?= ARTICLE_IMG_PATH,$article['image']?>" alt="" />
-        <h2><?= utf8_encode($article['nom'])?></h2>
-        <figcaption><?= utf8_encode($article['description'])?></figcaption>
+        <div id="slider_droit">
+        <h3><?= utf8_encode($article['nom'])?></h3>
+<!--        <figcaption>--><?//= utf8_encode($article['description'])?><!--</figcaption>-->
+
         <h4>Prix:</h4>
         <p><?= $article['prix']?></p>
-        <h4>Prix avec taxes:</h4>
-        <p><?= $article['prix avec taxes']?></p>
-        <h3>Economisez:</h3>
-        <p><?= utf8_encode ($article['economiser'])?></p>
-        <h4>Livraison:</h4>
-        <p><?= utf8_encode ($article['livraison'])?></p>
+<!--        <h4>Prix avec taxes:</h4>-->
+<!--        <p>--><?//= $article['prix avec taxes']?><!--</p>-->
+<!--        <h3>Economisez:</h3>-->
+        <h2><?= utf8_encode ($article['economiser'])?></h2>
+<!--        <h4>Livraison:</h4>-->
+<!--        <p>--><?//= utf8_encode ($article['livraison'])?><!--</p>-->
+        </div>
     </figure>
-
+    </a>
 
 <?php }?>
     </div>
