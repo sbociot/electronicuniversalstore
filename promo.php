9c1528f91d969127a28de7810c9b7a7c58a9577f
sbociot <stevebocicot@icloud.com>
2017-03-01 10:34:37 -0500

    nouveau fichier

--- promo.php
+++ promo.php
@@ -1,25 +1,27 @@
 <?php
 require_once'acces/acces_serv.php';
- var_dump($categorieS);
+// var_dump($categories);
 define('ARTICLE_IMG_PATH', 'images/');
 
 ?>
 
-    <div id="promotion">
+    <div id="slider">
 <?php foreach ($categories as $id => $article) { ?>
+
     <figure>
-        <h2><?= $article['nom']?></h2>
         <img src="<?= ARTICLE_IMG_PATH,$article['image']?>" alt="" />
+        <h2><?= utf8_encode($article['nom'])?></h2>
         <figcaption><?= utf8_encode($article['description'])?></figcaption>
         <h4>Prix:</h4>
         <p><?= $article['prix']?></p>
         <h4>Prix avec taxes:</h4>
         <p><?= $article['prix avec taxes']?></p>
         <h3>Economisez:</h3>
-        <p><?= $article['economiser']?></p>
+        <p><?= utf8_encode ($article['economiser'])?></p>
         <h4>Livraison:</h4>
-        <p><?= $article['livraison']?></p>
-
+        <p><?= utf8_encode ($article['livraison'])?></p>
     </figure>
-    </div>
+
+
 <?php }?>
+    </div>
