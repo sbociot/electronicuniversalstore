5f35c9aa863888a7f527a5452e0cdb6b5aa06b98
sbociot <stevebocicot@icloud.com>
2017-03-01 12:41:27 -0500

    responsive page acceuil

--- index.php
+++ index.php
@@ -10,11 +10,13 @@ define('ICON_PATH', 'images/icons/');
 <div class="image_categorie">
    <h1>Magasinez les AUBAINES par catégories</h1>
   <?php foreach ($myicon as $key => $value) {?>
+    <a href="aubaine_choix.php?aubaine=<?= $value['name'] ?>" >
       <div class="categorie_icon">
         <ul class="icon_wrapper">
           <li><img class="icon" src="<?= ICON_PATH,$value['image']?>" alt="<?= $value['name']?>"></li>
-          <li><a href="aubaine_choix.php?aubaine=<?= $value['name'] ?>"><?= $value['name']?></a></li>
+          <li><?= $value['name']?></li>
         </ul>
+    </a>
       </div>
   <?php }  ?>
 </div>
