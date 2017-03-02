68d11d954acc721de35ffdfe0060462bc747ed24
sbociot <stevebocicot@icloud.com>
2017-03-02 10:14:11 -0500

    formulaire login and logout

--- base/header.php
+++ base/header.php
@@ -1,5 +1,6 @@
 <?php
 define('ARTICLE_IMG_PATH', 'images/');
+require_once 'check_connect.php';
  ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -20,12 +21,26 @@ define('ARTICLE_IMG_PATH', 'images/');
                 <img class="logo" src="<?=ARTICLE_IMG_PATH?>electronicstoreu.png" alt="Electronic Universal Store">
             </a>
         </li>
-        <!--<li>
-          <form class="searbarengine" action="aubaine_choix.php" method="get">
-            <input type="search" name="search">
-            <input type="submit">
-          </form>
-        </li>-->
+        <li>
+            <?php if (is_logged_in()) { ?>
+                <span>Salut <?= $_SESSION[PS_USERNAME] ?> !</span>
+                <form name="logout" method="post">
+                    <input type="submit" name="logout_btn" value="Déconnecter" />
+                </form>
+            <?php } else { ?>
+                <form name="login" method="post">
+                    <input type="text" name="username" id="username"
+                           value="<?= array_key_exists('username',$_POST) ? $_POST['username'] : '' ?>"
+                    />
+                    <input type="password" name="password" id="password" value="" />
+                    <?php // Afficher un message dans le cas ou l'authentification n'a pas réussi
+                    if (array_key_exists('login_btn',$_POST)) { ?>
+                        <p>Le pseudo et le mot de passe fournis ne concordent pas.</p>
+                    <?php } ?>
+                    <input type="submit" name="login_btn" value="Connecter" />
+                </form>
+            <?php } ?>
+        </li>
         <li><img src="<?=ARTICLE_IMG_PATH?>online-store.png" alt="Panier"></li>
       </ul>
     </div>
