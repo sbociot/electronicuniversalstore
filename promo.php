96f7f069f93e308f017e6b7130cc895d35cc5efd
dchavezc <dervys75@gmail.com>
2017-03-01 19:13:30 -0500

    "responsive footer"

--- promo.php
+++ promo.php
@@ -6,7 +6,6 @@ require_once'acces/acces_serv.php';
 ?>
 
 <div id="slider">
-
 <?php foreach ($categories as $id => $article) { ?>
 
     <figure>
@@ -16,9 +15,7 @@ require_once'acces/acces_serv.php';
         <h3><?= utf8_encode($article['nom'])?></h3>
         <h4>Prix:</h4>
         <p><?= $article['prix']?></p>
-
         <h2><?= utf8_encode ($article['economiser'])?></h2>
-
         </div>
         </a>
     </figure>
