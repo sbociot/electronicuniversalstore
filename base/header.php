f27084c0a336ba2cbfdfa0add9f338549004044d
sbociot <stevebocicot@icloud.com>
2017-03-05 14:25:05 -0500

    "reconstruction du header et ajout du loggin"

--- base/header.php
+++ base/header.php
@@ -1,5 +1,6 @@
 <?php
 define('ARTICLE_IMG_PATH', 'images/');
+require_once 'check_connect.php';
  ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -17,16 +18,35 @@ define('ARTICLE_IMG_PATH', 'images/');
     <link  rel = "icon"  href = "images/favico/favicon-32x32.png" >
   </head>
   <body>
-    <div class="logo_container">
-      <ul class="header_wrapper">
-        <li><img class="logo" src="<?=ARTICLE_IMG_PATH?>electronicstoreu.png" alt="Electronic Universal Store"></li>
-        <!--<li>
-          <form class="searbarengine" action="aubaine_choix.php" method="get">
-            <input type="search" name="search">
-            <input type="submit">
-          </form>
-        </li>-->
-        <li><img src="<?=ARTICLE_IMG_PATH?>online-store.png" alt="Panier"></li>
-      </ul>
-    </div>
+  <header>
+      <div class="barner_header_wrapper">
+          <div class="barner">
+              <a href="index.php"><img class="logo" src="<?=ARTICLE_IMG_PATH?>electronicstoreu.png" alt="Electronic Universal Store"></a>
+          </div>
+          <div class="barner">
+              <img class="user_login_container" src="<?= ARTICLE_IMG_PATH ?>user.png" alt="Utilisateur">
+              <?php if (is_logged_in()) { ?>
+                  <span>Salut <?= $_SESSION[PS_USERNAME] ?> !</span>
+                  <form class="logout_user" name="logout" method="post">
+                      <input type="submit" name="logout_btn" value="Déconnecter"/>
+                  </form>
+              <?php } else { ?>
+                  <form class="login_user" name="login" method="post">
+                      <input type="text" name="username" id="username"
+                             value="<?= array_key_exists('username', $_POST) ? $_POST['username'] : '' ?>"
+                      />
+                      <input type="password" name="password" id="password" value=""/>
+                      <?php
+                      if (array_key_exists('login_btn', $_POST)) { ?>
+                          <p>Le pseudo et le mot de passe fournis ne concordent pas.</p>
+                      <?php } ?>
+                      <input type="submit" name="login_btn" value="Connecter"/>
+                  </form>
+              <?php } ?>
+          </div>
+          <div class="barner">
+              <img class="logo" src="<?=ARTICLE_IMG_PATH?>online-store.png" alt="Electronic Universal Store">
+          </div>
+      </div>
+  </header>
       <?php require_once 'base/menu.php'; ?>
