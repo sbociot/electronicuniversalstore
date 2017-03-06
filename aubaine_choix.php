29183d27dbb9b972495aa4beecc45ad86561a22e
sbociot <stevebocicot@icloud.com>
2017-03-05 19:25:20 -0500

    modif de promo et affichage des produit dans une seule page

--- aubaine_choix.php
+++ aubaine_choix.php
@@ -8,19 +8,30 @@
 /*$userSearch = ucfirst($_GET['search']);*/
 /*var_dump($userSearch);*/
  ?>
-
-<div class="aubaine_choix_user_wrapper">
-    <h1><?= $_GET['aubaine']  ?></h1>
-  <?php
-    foreach ($aubainesCategorie as $key => $value) {
-      if (array_key_exists('aubaine', $_GET) && in_array($_GET['aubaine'], $value)) {?>
-          <div class="aubaine_choix_user">
-        <h2><?=$value['nom']?></h2>
-        <img src="<?= ARTICLE_IMG_PATH,$value['image']?>" alt="<?=$value['nom']?>"/>
-        <p><a href="user_choice.php?aubaine=<?=$value['nom']?>"><?= $value['prix'] ?></a> </p>
-</div>
-      <?php }?>
-    <?php }?>
-</div>
+    <div class="aubaine_choix_user_wrapper">
+        <h1><?php if (array_key_exists('aubaine' , $_GET)){
+                echo  $_GET['aubaine'];
+            }else {
+                echo $_GET['promo'];
+            }  ?></h1>
+        <?php
+        foreach ($aubainesCategorie as $key => $value) {
+            if (array_key_exists('aubaine', $_GET) && in_array($_GET['aubaine'], $value)) {?>
+                <div class="aubaine_choix_user">
+                    <h2><?=$value['nom']?></h2>
+                    <img src="<?= ARTICLE_IMG_PATH,$value['image']?>" alt="<?=$value['nom']?>"/>
+                    <p><a href="user_choice.php?aubaine=<?=$value['nom']?>"><?= $value['prix'] ?></a> </p>
+                </div>
+            <?php }else foreach ($categories as $key => $value){
+                if(array_key_exists('promo' , $_GET) && in_array($_GET['promo'] , $value)){?>
+                    <div class="aubaine_choix_user">
+                        <h2><?= $value['nom']?></h2>
+                        <img src="<?= ARTICLE_IMG_PATH,$value['image']?>" alt="<?=$value['nom']?>"/>
+                        <p><a href="user_choice.php?aubaine=<?= $value['nom']?>"><?= $value['prix'] ?></a> </p>
+                    </div>
+                <?php }
+            }?>
+        <?php }?>
+    </div>
 <?php
 require_once 'base/footer.php';
--- promo.php
+++ promo.php
@@ -9,16 +9,15 @@ require_once'acces/acces_serv.php';
     <?php foreach ($categories as $id => $article) { ?>
 
         <figure>
-            <a href="aubaine_choix.php?=promo=<?= utf8_encode($article['nom'])?>">
-                <img src="<?= ARTICLE_IMG_PATH,$article['image']?>" alt=""/>
+            <a href="aubaine_choix.php?promo=<?= $article['nom']?>">
+                <img src="<?= ARTICLE_IMG_PATH,$article['image']?>" alt="<?=$article['nom']?>"/>
                 <div class="slider_droit">
-                    <h3><?= utf8_encode($article['nom'])?></h3>
-                    <h4>Prix:</h4>
-                    <p><?= $article['prix']?></p>
-                    <h2><?= utf8_encode ($article['economiser'])?></h2>
-
-        </div>
-        </a>
-    </figure>
-<?php }?>
+                    <h3><?= $article['nom']?></h3>
+                    <p>Prix : <?= $article['prix']?></p>
+                    <h2><?= $article['economiser']?></h2>
+                </div>
+            </a>
+        </figure>
+    <?php }?>
 </div>
+
