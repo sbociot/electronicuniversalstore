48dd358c2817c55295d9df35fdca354e8c41aad5
dchavezc <dervys75@gmail.com>
2017-03-06 11:27:16 -0500

    "coregir error sur le nom d'articles promo <?= utf8_encode($article['nom'])?>"

--- promo.php
+++ promo.php
@@ -9,11 +9,9 @@ require_once'acces/acces_serv.php';
     <?php foreach ($categories as $id => $article) { ?>
 
         <figure>
-            <a href="aubaine_choix.php?promo=<?= $article['nom']?>">
+            <a href="aubaine_choix.php?promo=<?= utf8_encode($article['nom'])?>">
                 <img src="<?= ARTICLE_IMG_PATH,$article['image']?>" alt="<?=$article['nom']?>"/>
                 <div class="slider_droit">
-
-              
                     <h3><strong><?= utf8_encode($article['nom'])?></strong></h3>
                     <h4>Prix:</h4>
                     <p><?= $article['prix']?></p>
